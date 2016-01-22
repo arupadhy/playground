12089f5e9956c026582f323bc00d241fb69caf93
arupadhy <arvind.charlotte@gmail.com>
2016-01-22 14:56:13 -0500

    [arvind]-angular setup with spring maven jetty plugin..basic structure

--- spring-web-maven/src/main/java/com/arvind/app/controller/HTMLPageController.java
+++ spring-web-maven/src/main/java/com/arvind/app/controller/HTMLPageController.java
@@ -1,19 +1,15 @@
 package com.arvind.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HTMLPageController {
 
-    @RequestMapping(value = "/view", method = RequestMethod.GET)
-    public String view(Model model) {
-        model.addAttribute("name", "Some name to be displayed");
-        return "view";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePage() {
+        return "main";
     }
 
 }
--- spring-web-maven/src/main/java/com/arvind/app/web/config/SpringWebConfig.java
+++ spring-web-maven/src/main/java/com/arvind/app/web/config/SpringWebConfig.java
@@ -6,32 +6,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.arvind.app"})
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-//    }
-	
-	@Override
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath/WEB-INF/**");
+    }
+
+    @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
+        configurer.enable();
     }
 
     @Bean
     public ViewResolver viewResolver() {
-    	InternalResourceViewResolver resolver =
+        InternalResourceViewResolver resolver =
                 new InternalResourceViewResolver();
-        resolver.setPrefix("/static/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        //resolver.setViewClass(JstlView.class);
         return resolver;
     }
 }
